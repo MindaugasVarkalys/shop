e852557689a11e4de07566f9eea32433f0c89af0
Mindaugas Varkalys <varkalys.mindaugas@gmail.com>
2018-09-17 23:29:40 +0200

    Reverted config changes

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 class config {
 
     const DB_SERVER = 'localhost';
-    const DB_NAME = 'tautvio_shopas';
+    const DB_NAME = 'shop';
     const DB_USERNAME = 'root';
     const DB_PASSWORD = '';
     const NUMBER_OF_ROWS_IN_PAGE = 10;
